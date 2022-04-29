e73339aadb1a71f2f7522841c274310b3ac6f036
koo099 <koo8930@gmail.com>
2022-04-29 23:23:29 +0900

    4/29

---
+++ js/slot.js
@@ -0,0 +1,52 @@
+"use strict";
+{
+  class Panel {
+    constructor() {
+      const section = document.createElement("section");
+      section.classList.add("panel");
+
+      this.img = document.createElement("img");
+      this.img.src = this.getRandomImage();
+
+      this.timeoutId = undefined;
+
+      this.stop = document.createElement("div");
+      this.stop.textContent = "stop";
+      this.stop.classList.add("stop");
+      this.stop.addEventListener('click',()=>{
+        clearTimeout(this.timeoutId);
+      });
+
+      section.appendChild(this.img);
+      section.appendChild(this.stop);
+
+      const main = document.querySelector("main");
+      main.appendChild(section);
+    }
+
+    getRandomImage(){
+      const images =[
+        'Myslotmachine/img/seven.png',
+        'Myslotmachine/img/bell.png',
+        'Myslotmachine/img/cherry.png',
+      ];
+      return images[Math.floor(Math.random()*images.length)]
+    }
+
+    spin(){
+      this.img.src =this.getRandomImage();
+      this.timeoutId=setTimeout (()=>{
+        this.spin();
+      },50);
+    }
+  }
+
+  const panels = [new Panel(), new Panel(), new Panel()];
+
+  const spin = document.getElementById("spin");
+  spin.addEventListener("click", () => {
+    panels.forEach((panel) => {
+      panel.spin();
+    });
+  });
+}
